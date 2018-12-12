5ccb01e41a19d5199454cbd59d44da9468fbe33e
Max Livingston <maxliving@gmail.com>
2018-12-12 10:03:56 -0400

    Add git commit back in

--- _update_site.sh
+++ _update_site.sh
@@ -4,10 +4,13 @@
 
 # Requires you to have set up your ssh key (see http://www.thegeekstuff.com/2008/11/3-steps-to-perform-ssh-login-without-password-using-ssh-keygen-ssh-copy-id/)
 
-## Build first, b/c we need to generate the site files before git pushing.
+# Build first, b/c we need to generate the site files before git pushing.
 jekyll build
 
-## Push
+# Commit the site files that we just generated
+git commit -am --allow-empty "Auto-commit for site files"
+
+# Push
 git push
 
 ssh maxlivin@maxlivingston.org "cd gitsite; git pull origin master; cp -r _site/* ../public_html/"
