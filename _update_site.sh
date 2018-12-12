8c0e2963951d51b7c96620da75e0cd787855ef89
Max Livingston <maxliving@gmail.com>
2018-12-12 10:05:50 -0400

    Set euxo

--- _update_site.sh
+++ _update_site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euox pipefail
+
 # Performs the multi-step process of updating my public website once I've made all the changes I want to locally.
 
 # Requires you to have set up your ssh key (see http://www.thegeekstuff.com/2008/11/3-steps-to-perform-ssh-login-without-password-using-ssh-keygen-ssh-copy-id/)
@@ -8,7 +10,7 @@
 jekyll build
 
 # Commit the site files that we just generated
-git commit -am --allow-empty "Auto-commit for site files"
+git commit --allow-empty -am  "Auto-commit for site files"
 
 # Push
 git push
